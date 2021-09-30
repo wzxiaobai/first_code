35570d5e6684448fae3e253944a54b3a2d0a7bcb
wzxiaobai <392719932@qq.com>
2021-09-30 11:02:07 +0800

    添加hello_word.py文件

--- hello_word.py
+++ hello_word.py
@@ -1,1 +1,9 @@
 print('hello,world')
+import os
+##
+print('hello,world')
+print('hello,world')
+print('hello,world')
+print('hello,world')
+print('hello,world')
+print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')print('hello,world')
